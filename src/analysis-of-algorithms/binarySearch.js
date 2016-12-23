d58bdf7dafb15927f20abde5f8790e124774b830
Christopher Tham <chriswt91@gmail.com>
2016-12-22 21:33:40 -0800

    Implement binary search

---
+++ src/analysis-of-algorithms/binarySearch.js
@@ -0,0 +1,21 @@
+const binarySearch = (sortedArray, key) => {
+  let low = 0;
+  let high = sortedArray.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (sortedArray[mid] === key) {
+      return mid; // Found the index of the key
+    } else if (key > sortedArray[mid]) {
+      low = mid + 1; // Go right
+    } else if (key < sortedArray[mid]) {
+      high = mid - 1; // Go left
+    }
+  }
+
+  return -1; // Key not found
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([38, 88, 199, 512, 600], 4)) // -1
