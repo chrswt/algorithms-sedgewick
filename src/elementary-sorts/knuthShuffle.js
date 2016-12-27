882d4388a6d82a710693ed796cb3bb6ecfebaee5
Christopher Tham <chriswt91@gmail.com>
2016-12-27 01:28:40 -0800

    Add note regarding unbiased shuffles

--- src/elementary-sorts/knuthShuffle.js
+++ src/elementary-sorts/knuthShuffle.js
@@ -1,6 +1,12 @@
 const knuthShuffle = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    // Generate a random number between 0 and i
+    /*
+     * Generate a random number between 0 and i.
+     * IMPORTANT: do not generate a random number between 0 and length, because
+     * this would generate N^N possibilities, whereas a shuffle only has N!
+     * permutations. Randomizing from 0 to length would result in a biased
+     * shuffling algorithm.
+     */
     let rnd = Math.floor((Math.random() * (i + 1)));
 
     // Swap the element in position i with the element in position rnd
