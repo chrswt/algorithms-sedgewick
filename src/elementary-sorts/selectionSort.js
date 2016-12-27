cb8f9b3e56482f7ca193358f6d26d4eceeb6b596
Christopher Tham <chriswt91@gmail.com>
2016-12-26 23:11:24 -0800

    Implement selection sort

---
+++ src/elementary-sorts/selectionSort.js
@@ -0,0 +1,22 @@
+const selectionSort = (arr) => {
+  for (var i = 0; i < arr.length - 1; i++) {
+    let min = i;
+
+    for (var j = i + 1; j < arr.length; j++) {
+      // Find the minimum value in the unsorted sub-array
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    // Swap minimum element with the i'th element to maintain sorted sub-array
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([0, -5, 23, -2, 19, 3, 7]));
+// [-5, -2, 0, 3, 7, 19, 23]
+console.log(selectionSort(['d', 'aa', 'ba', 'ab', 'da', 'a']));
+// ['a', 'aa', 'ab', 'ba', 'd', 'da']
