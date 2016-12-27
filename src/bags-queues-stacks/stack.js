70449a4186ea499061ecc7e5e4fd289ccce41f44
Christopher Tham <chriswt91@gmail.com>
2016-12-26 16:33:30 -0800

    Refactor to prevent loitering

--- src/bags-queues-stacks/stack.js
+++ src/bags-queues-stacks/stack.js
@@ -13,17 +13,22 @@ class Stack {
   }
 
   push(item) {
-    // use num to index the array, then increment num
+    // Use num to index the array, then increment num
     this.storage[this.num++] = item;
   }
 
   pop() {
-    // decrement num then use it to index the array
-    return this.storage[--this.num];
+    /*
+     * This allows us to decrement the counter, then use it to index the array.
+     * This implementation also removes reference to the deleted object.
+     */
+    let item = this.storage[--this.num];
+    this.storage[this.num] = null;
+    return item;
   }
 }
 
-let s = new Stack()
+let s = new Stack();
 console.log(s.isEmpty()); // true
 s.push(1);
 s.push(2);
