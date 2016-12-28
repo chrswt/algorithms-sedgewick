4910f03cad42e03ec78cad39fb7b77c3d002a402
Christopher Tham <chriswt91@gmail.com>
2016-12-27 23:08:47 -0800

    Implement drawTo API method

--- src/mergesort/point.js
+++ src/mergesort/point.js
@@ -23,27 +23,54 @@ class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.scale = 41.25;
   }
 
   draw() {
-    const canvas = document.getElementById('graph');
-    const ctx = canvas.getContext('2d');
-    const radius = 1;
-    // Scaled to fit 0 - 33,000 into 800px by 800px canvas
-    const centerX = this.x / 41.25;
-    const centerY = this.y / 41.25;
+    if (typeof(document) !== 'undefined') {
+      const canvas = document.getElementById('graph');
+      const ctx = canvas.getContext('2d');
+      const radius = 1;
 
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.linewidth = 0.2;
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
+      // Scaled to fit 0 - 33,000 into 800px by 800px canvas
+      const centerX = this.x / this.scale;
+      const centerY = this.y / this.scale;
+
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = 'blue';
+      ctx.fill();
+      ctx.linewidth = 0.2;
+      ctx.strokeStyle = 'black';
+      ctx.stroke();
+    }
+  }
+
+  drawTo(point) {
+    if (typeof(document) !== 'undefined') {
+      const canvas = document.getElementById('graph');
+      const ctx    = canvas.getContext('2d');
+
+      const centerX = this.x / this.scale;
+      const centerY = this.y / this.scale;
+      const destX   = point.x / this.scale;
+      const destY   = point.y / this.scale;
+
+      ctx.beginPath();
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(destX, destY);
+      ctx.stroke();
+    }
+  }
+
+  toString() {
+    return [this.x, this.y];
   }
 }
 
 const p = new Point(1000, 1000);
 p.draw();
-const q = new Point(1100, 1100);
+const q = new Point(18000, 17100);
 q.draw();
+p.drawTo(q);
+console.log(q.toString());
